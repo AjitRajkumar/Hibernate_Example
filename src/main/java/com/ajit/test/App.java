6b73199b56e8b2bdb72ecae84dbfcf2bd84c70f6
AjitRajkumar <ajitrajkumar45@gmail.com>
2024-04-22 22:27:54 +0530

    hibernate project is created

---
+++ src/main/java/com/ajit/test/App.java
@@ -0,0 +1,26 @@
+package com.ajit.test;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setName("Balaji");
+		s1.setRollno(20);
+		Configuration con = new Configuration().configure().addAnnotatedClass(Student.class);
+		SessionFactory ssf = con.buildSessionFactory();
+		Session session = ssf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(s1);
+		System.out.println("Hello World!");
+		Student test = session.get(Student.class, 12);
+		System.out.println(test);
+		tx.commit();
+	}
+}
